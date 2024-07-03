def28e7b46c9dd5d38c6299de5dc3a8a4fdc2ff1
AnarRussia1219 <ft2j5fqf@duck.com>
2024-07-03 16:29:43 +0100

    setup webpack and sass

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "src/index.js"),
+    output: {
+        filename: "scripts.js",
+        path: path.resolve(__dirname, "src-build"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+};
